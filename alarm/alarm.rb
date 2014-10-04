5e6b55f2efc2abc4ac158fa2dd44c002def9e1c8
Inbar <inbar.fried@tufts.edu>
2014-10-04 12:17:03 -0400

    added packetFu packet library to parse packets from live stream

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -1,5 +1,6 @@
 require 'packetfu'
 require 'Slop'
+require 'snort-rule'
 
 $alarm_instance_number = 1
 
@@ -20,9 +21,23 @@ end
 # capture live stream of network packets
 
 pkt_array = PacketFu::Capture.new(:start => true, :iface => 'en1', :promisc => true)
-# XMAS scan
+pkt_array.stream.each do |p|
+  pkt = PacketFu::Packet.parse(p)
+  if pkt.is_ip? and pkt.is_tcp? 
+    if pkt.tcp_flags.syn == 0 and pkt.tcp_flags.ack == 0 and pkt.tcp_flags.psh == 0 and pkt.tcp_flags.urg == 0 and pkt.tcp_flags.rst == 0 and pkt.tcp_flags.fin == 0
+    	raiseAlarm('NULL', pkt.ip_saddr, pkt.proto, pkt.payload)
+	  elsif pkt.tcp_flags.urg == 1 and pkt.tcp_flags.fin == 1 and pkt.tcp_flags.psh == 1
+		  raiseAlarm('XMAS', pkt.ip_saddr, pkt.proto, pkt.payload)
+    end
+  end
+end
+
 pkt_array.show_live(:filter => 'tcp[tcpflags] & (tcp-push & tcp-fin & tcp-urg) == (tcp-push & tcp-fin & tcp-urg)')
 
+def raiseAlarm(attack, source_IP, protocol, payload)
+  puts " #{$alarm_instance_number}. ALERT: #{attack} is detected from #{source_IP} (#{protocol}) (#{payload})!"
+  $alarm_instance_number += 1
+end
 
 =begin
 # convert caught packet streams from string to PacketFu packet
@@ -36,8 +51,10 @@ while caught == false do
 end
 =end
 
-def raiseAlarm(attack, source_IP, protocol, payload)
-	puts " #{$alarm_instance_number}. ALERT: #{attack} is detected from #{source_IP} (#{protocol}) (#{payload})!"
-	$alarm_instance_number += 1
-end
+      #print "Source Addr: #{pkt.ip_saddr}\n"
+      #print "Destination Addr: #{pkt.ip_daddr}\n"
+      #print "Destination Port: #{pkt.tcp_dport}\n"
+      #print "TCP Options: #{pkt.tcp_options}\n"
+      #print "TCP SYN?: #{pkt.tcp_flags.syn}\n"
+      #print "TCP ACK?: #{pkt.tcp_flags.ack}\n"
 
