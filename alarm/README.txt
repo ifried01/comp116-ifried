8b77dd663fb13c399b901c36a2506725bf726eac
Inbar <inbar.fried@tufts.edu>
2014-10-07 15:32:39 -0400

    finished assignment

--- alarm/README.txt
+++ alarm/README.txt
@@ -14,18 +14,24 @@ Assignment 2 - Incident Alarm with Ruby and PacketFu
 
 4) Are the heuristics used in this assignment to determine inicidents "even that good"?
 	~ No. Detecting ShellCode, for example, is definitely not a direct and
-		one-solution issue. We need to detect hexadecimal in the , but
+		one-solution issue. We need to detect hexadecimal in the payload, but
 		hexadecimal shows up in many packages that are not actuall ShellCode.
 		In this implementation I only match on payloads that begin with
 		hexadecimal exclusively.
 
-5) If you have spare time in the future, what would you add to the program or do differently with regards to detecting incidents?
-	~ I would add further specifications to checks like credit card detection in
-	  order to detect with payloads that are really sophisticated.
+5) If you have spare time in the future, what would you add to the program or do
+   differently with regards to detecting incidents?
+	~ I would add further specifications to detections like credit card detection in
+	  order to detect payloads that are really sophisticated, such as polymorphic code.
 	  I would also add a learning method that would block certain IPs after getting
 	  attempted intrusions from the specific IP.
 	  
 	  On another note, once an incident is detected, it might be too late
-	  to treat the issue. To fix this issue, we need to address the cause.
+	  to treat the issue. To fix this issue, we need to address the cause of the
+	  vulnerability.
 
-
+NOTE
+	- In my implementation of the web server log scanning, if a single packet is
+	  both an HTTP 400-range issue, and an NMAP issue, for example, I will raise
+	  two alerts. One alert will be regarding the HTTP 400-range issue and the other
+	  alert will be regarding the NMAP issue.
--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -7,6 +7,7 @@ Assignment 2 - Alarm
 
 require 'packetfu'
 require 'Slop'
+require 'base64'
 
 # regex for visa, mastercard, dicover card, and american Express credit cards
 $visa        = /4\d{3}(\s|-)?\d{4}(\s|-)?\d{4}(\s|-)?\d{4}/
@@ -29,10 +30,10 @@ def analyzeLiveNetworkTraffic()
     if pkt.is_ip? and pkt.is_tcp?
       # when all the flags are set to 0, it's a NULL scan
       if pkt.tcp_flags.syn == 0 and pkt.tcp_flags.ack == 0 and pkt.tcp_flags.psh == 0 and pkt.tcp_flags.urg == 0 and pkt.tcp_flags.rst == 0 and pkt.tcp_flags.fin == 0
-      	raiseAlarm('NULL scan is detected', pkt.ip_saddr, pkt.proto, pkt.payload)
+      	raiseAlarm('NULL scan is detected', pkt.ip_saddr, pkt.proto, Base64.encode64(pkt.payload))
       # when the 'urg', 'psh', and 'fin' flags are set to 1, it's a XMAS scan
   	  elsif pkt.tcp_flags.urg == 1 and pkt.tcp_flags.fin == 1 and pkt.tcp_flags.psh == 1
-	 	   raiseAlarm('XMAS scan is detected', pkt.ip_saddr, pkt.proto, pkt.payload)
+	 	   raiseAlarm('XMAS scan is detected', pkt.ip_saddr, pkt.proto, Base64.encode64(pkt.payload))
       else
         checkForCreditCards(pkt)
       end
@@ -42,7 +43,7 @@ end
 
 def checkForCreditCards(pkt)
   if pkt.payload.match($visa) or pkt.payload.match($master) or pkt.payload.match($discover) or pkt.payload.match($americanExp)
-    raiseAlarm('Credict card leaked in the clear', pkt.ip_saddr, pkt.proto, pkt.payload)
+    raiseAlarm('Credict card leaked in the clear', pkt.ip_saddr, 'HTTP', Base64.encode64(pkt.payload))
   end
 end
 
@@ -54,10 +55,12 @@ def analyzeWebServerLog(log_file)
       source_IP     = line_contents[0]
       if line.match($HTTP400Err)
         raiseAlarm('HTTP error is detected', source_IP, 'HTTP', payload)
+      end
       if line.match($nmapScan)
-        raiseAlarm('NMAP scan is detected', source_IP, 'SOCKS', )
+        raiseAlarm('NMAP scan is detected', source_IP, 'HTTP', payload)
+      end
       if payload.match($shellCode)
-        raiseAlarm('Shellcode is detected', source_IP, , )
+        raiseAlarm('Shellcode is detected', source_IP, 'HTTP', payload)
       end
     end
   end
@@ -77,32 +80,9 @@ end
 
 # if the -r flag was provided
 if opts.web_server?
-  # web_server_file = opts[:web_server]
-  puts "Running with web server log"
+  ''' Running with web server log '''
   analyzeWebServerLog(opts[:web_server])
 else
-  puts "Running live network traffic"
+  ''' Running live network traffic '''
   analyzeLiveNetworkTraffic()
 end
-
-#pkt_array.show_live(:filter => 'tcp[tcpflags] & (tcp-push & tcp-fin & tcp-urg) == (tcp-push & tcp-fin & tcp-urg)')
-
-=begin
-# convert caught packet streams from string to PacketFu packet
-caught = false
-while caught == false do
-	pkt_array.stream.each do |p|
-		if PacketFu::Packet.has_data?
-			pkt = PacketFu::Packet.parse(p)
-		end
-	end
-end
-=end
-
-      #print "Source Addr: #{pkt.ip_saddr}\n"
-      #print "Destination Addr: #{pkt.ip_daddr}\n"
-      #print "Destination Port: #{pkt.tcp_dport}\n"
-      #print "TCP Options: #{pkt.tcp_options}\n"
-      #print "TCP SYN?: #{pkt.tcp_flags.syn}\n"
-      #print "TCP ACK?: #{pkt.tcp_flags.ack}\n"
-
